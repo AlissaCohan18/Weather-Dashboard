e8c93d9934ef525fabdb5d244c2eff33cca5c5ac
Alissa Cohan <alissacohan18@gmail.com>
2022-11-27 16:05:52 -0500

    format vetted in html, pseudo code outline in .js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// register for API key
+
+//'search' event listener
+//function that pulls in city from search field
+// convert city name to geographical coordinates prior to passing in
+// retrieve weather data **FOR 6 DAYS
+// creates cards with current data pulled in
+
+// save city object (name & coordinates) into local storage
+// create new <button> for history element
+
+// represent city name, date, icon, Temp, Wind, Humidity for each of the 6 days
+
+// history event listener, rerun pull-weather function()
