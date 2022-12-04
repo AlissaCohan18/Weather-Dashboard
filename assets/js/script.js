90bc67b30aa28d93b71bd81507c174dbea6a2e05
Alissa Cohan <alissacohan18@gmail.com>
2022-12-04 16:59:29 -0500

    eventListeners on search and history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 // declare variables
-var city = "Orlando";
+var city = "orlando";
 var apiKey = "2d7a4c5927b560838f4ca31749bda05b";
 var latCoord;
 var lonCoord;
+
+var idCitySearchEl = document.getElementById("idCitySearch");
+
 var cityNameEl = document.getElementById("cityName");
 var tempEl = document.getElementById("temp0");
 var windEl = document.getElementById("wind0");
@@ -32,6 +35,19 @@ var dateEl5 = document.getElementById("date5");
 var tempEl5 = document.getElementById("temp5");
 var windEl5 = document.getElementById("wind5");
 var humidityEl5 = document.getElementById("humidity5");
+
+//recalls and loads history from local storage
+var allCities = JSON.parse(localStorage.getItem("cities"));
+if (!allCities) {
+  allCities = [];
+} else {
+  for (i = 0; i < allCities.length; i++) {
+    var historyEl = document.createElement("button");
+    historyEl.setAttribute("class", "btn btn-lg btn-secondary");
+    historyEl.innerText = allCities[i];
+    document.querySelector(".history").appendChild(historyEl);
+  }
+}
 
 //fetch coordinates when passing city name into function
 var fetchCoordinates = function (city) {
@@ -70,11 +86,26 @@ function fetchWeather(data1) {
   //Display current day
   var now = moment().format("MMMM Do YYYY");
   $(currentDay).text(now);
+
+  //Function to retrieve the data entered in search
+  // function search() {
+  //         this.fetchWeather(document.querySelector(".search_btn").value);
+  //       }
+
+  //add only new cities to history array and create a button
+  if (allCities.includes(data1.city.name) == false) {
+    allCities.push(data1.city.name);
+    localStorage.setItem("cities", JSON.stringify(allCities));
+    historyEl = document.createElement("button");
+    historyEl.setAttribute("class", "btn btn-lg btn-secondary");
+    historyEl.innerText = data1.city.name;
+    document.querySelector(".history").appendChild(historyEl);
+  }
+
   //converts all of the weather api data to text on the page for each day
   tempEl.textContent = "Temp: " + data1.list[0].main.temp;
   windEl.textContent = "Wind: " + data1.list[0].main.humidity;
   humidityEl.textContent = "Humidity: " + data1.list[0].wind.speed;
-  console.log(data1);
 
   dateEl1.textContent = moment().add(1, "days").format("l");
   tempEl1.textContent = "Temp: " + data1.list[1].main.temp;
@@ -102,10 +133,17 @@ function fetchWeather(data1) {
   humidityEl5.textContent = "Humidity: " + data1.list[5].wind.speed;
 }
 
-//'search' event listener
-//function that pulls in city from search field
-
-// save city object (name & coordinates) into local storage
-// create new <button> for history element
-
 // TODO: Add icon for each of the 6 days
+
+$(".search_btn").on("click", function () {
+  fetchCoordinates(idCitySearch.value);
+  $("#idCitySearch").val("");
+});
+
+// Event listener for click on historical search buttons
+$(".history").on("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName === "BUTTON") {
+    fetchCoordinates(event.target.innerText);
+  }
+});
