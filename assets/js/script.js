387565c54badb8075adc0619feb113c97bf7bdc5
Alissa Cohan <alissacohan18@gmail.com>
2022-12-04 02:13:42 -0500

    pull weather api and feed response into cards displayed on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,111 @@
-// register for API key
+// declare variables
+var city = "Orlando";
+var apiKey = "2d7a4c5927b560838f4ca31749bda05b";
+var latCoord;
+var lonCoord;
+var cityNameEl = document.getElementById("cityName");
+var tempEl = document.getElementById("temp0");
+var windEl = document.getElementById("wind0");
+var humidityEl = document.getElementById("humidity0");
+
+var dateEl1 = document.getElementById("date1");
+var tempEl1 = document.getElementById("temp1");
+var windEl1 = document.getElementById("wind1");
+var humidityEl1 = document.getElementById("humidity1");
+
+var dateEl2 = document.getElementById("date2");
+var tempEl2 = document.getElementById("temp2");
+var windEl2 = document.getElementById("wind2");
+var humidityEl2 = document.getElementById("humidity2");
+
+var dateEl3 = document.getElementById("date3");
+var tempEl3 = document.getElementById("temp3");
+var windEl3 = document.getElementById("wind3");
+var humidityEl3 = document.getElementById("humidity3");
+
+var dateEl4 = document.getElementById("date4");
+var tempEl4 = document.getElementById("temp4");
+var windEl4 = document.getElementById("wind4");
+var humidityEl4 = document.getElementById("humidity4");
+
+var dateEl5 = document.getElementById("date5");
+var tempEl5 = document.getElementById("temp5");
+var windEl5 = document.getElementById("wind5");
+var humidityEl5 = document.getElementById("humidity5");
+
+//fetch coordinates when passing city name into function
+var fetchCoordinates = function (city) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey
+  )
+    .then((response) => response.json())
+    .then((data) => displayWeather(data));
+};
+
+fetchCoordinates(city);
+
+//pass in coordinates of city to display weather data
+function displayWeather(data) {
+  lonCoord = data.coord.lon;
+  latCoord = data.coord.lat;
+
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      latCoord +
+      "&lon=" +
+      lonCoord +
+      "&units=imperial&appid=" +
+      apiKey
+  )
+    .then((response) => response.json())
+    .then((data1) => fetchWeather(data1));
+}
+
+//function call to display weather data to page
+function fetchWeather(data1) {
+  cityNameEl.textContent = data1.city.name;
+  //Display current day
+  var now = moment().format("MMMM Do YYYY");
+  $(currentDay).text(now);
+  //converts all of the weather api data to text on the page for each day
+  tempEl.textContent = "Temp: " + data1.list[0].main.temp;
+  windEl.textContent = "Wind: " + data1.list[0].main.humidity;
+  humidityEl.textContent = "Humidity: " + data1.list[0].wind.speed;
+  console.log(data1);
+
+  dateEl1.textContent = moment().add(1, "days").format("l");
+  tempEl1.textContent = "Temp: " + data1.list[1].main.temp;
+  windEl1.textContent = "Wind: " + data1.list[1].main.humidity;
+  humidityEl1.textContent = "Humidity: " + data1.list[1].wind.speed;
+
+  dateEl2.textContent = moment().add(2, "days").format("l");
+  tempEl2.textContent = "Temp: " + data1.list[2].main.temp;
+  windEl2.textContent = "Wind: " + data1.list[2].main.humidity;
+  humidityEl2.textContent = "Humidity: " + data1.list[2].wind.speed;
+
+  dateEl3.textContent = moment().add(3, "days").format("l");
+  tempEl3.textContent = "Temp: " + data1.list[3].main.temp;
+  windEl3.textContent = "Wind: " + data1.list[3].main.humidity;
+  humidityEl3.textContent = "Humidity: " + data1.list[3].wind.speed;
+
+  dateEl4.textContent = moment().add(4, "days").format("l");
+  tempEl4.textContent = "Temp: " + data1.list[4].main.temp;
+  windEl4.textContent = "Wind: " + data1.list[4].main.humidity;
+  humidityEl4.textContent = "Humidity: " + data1.list[4].wind.speed;
+
+  dateEl5.textContent = moment().add(5, "days").format("l");
+  tempEl5.textContent = "Temp: " + data1.list[5].main.temp;
+  windEl5.textContent = "Wind: " + data1.list[5].main.humidity;
+  humidityEl5.textContent = "Humidity: " + data1.list[5].wind.speed;
+}
 
 //'search' event listener
 //function that pulls in city from search field
-// convert city name to geographical coordinates prior to passing in
-// retrieve weather data **FOR 6 DAYS
-// creates cards with current data pulled in
 
 // save city object (name & coordinates) into local storage
 // create new <button> for history element
 
-// represent city name, date, icon, Temp, Wind, Humidity for each of the 6 days
-
-// history event listener, rerun pull-weather function()
+// TODO: Add icon for each of the 6 days
